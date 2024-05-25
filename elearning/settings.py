17cddd065d6a581230c9178f19b1c1fbdb48a087
tempestpulse <bulsar@tutanota.com>
2024-05-25 20:07:24 +0200

    add simplejwt

--- elearning/settings.py
+++ elearning/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ],
+# }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 # Application definition
 
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CustomUserCreate, CustomUserDetail
 
 urlpatterns = [
-    path('user/create/', CustomUserCreate.as_view(), name='user-create'),
+    path('user/register/', CustomUserCreate.as_view(), name='user-create'),
     path('user/<slug:slug>/', CustomUserDetail.as_view(), name='user-detail'),
 ]
