0dfc3307eeaa9a465230a4ae51a113d7cd1adaaf
tempestpulse <bulsar@tutanota.com>
2024-05-25 22:20:04 +0200

    add teacher permissions

--- courses/permissions.py
+++ courses/permissions.py
@@ -1,2 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
+
+class IsTeacherOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_teacher
+
+class IsCourseTeacherOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.teacher == request.user
